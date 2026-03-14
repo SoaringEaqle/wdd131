e08a1dbfbad2bf8d2d8c5096327c866693b34b1a
SoaringEaqle <158627775+SoaringEaqle@users.noreply.github.com>
2026-03-14 13:47:23 -0600

    Prove w10

--- ponder/sortfilter.js
+++ ponder/sortfilter.js
@@ -175,3 +175,97 @@ const hikes = [
 
 
 
+function search() {
+
+    let hikeQuery = document.querySelector('#search').value;
+
+    let filteredHikes = hikes.filter(function(hike){
+        return (
+            hike.name.toLowerCase().includes(hikeQuery.toLowerCase()) ||
+            hike.description.toLowerCase().includes(hikeQuery.toLowerCase()) ||
+            hike.tags.find(tag => tag.toLowerCase().includes(hikeQuery.toLowerCase()))
+        );
+    })
+
+    function compareHikes(a,b) {
+        if (a.difficulty < b.difficulty) {
+            return -1;
+        } else if (a.difficulty > b.difficulty) {
+            return 1;
+        }
+        return 0;
+    }
+
+    let sortedHikes = filteredHikes.sort(compareHikes);
+
+    // clear out any previous content
+    hikeContainer.innerHTML = '';
+    // output onto screen
+    sortedHikes.forEach(function(hike){
+        renderHike(hike);
+    })
+}
+
+let hikeContainer = document.querySelector('#hike-container');
+let input = document.querySelector('#search');
+
+let button = document.querySelector('button');
+
+button.addEventListener('click', search);
+
+// for the enter key to work on search - not just clicking the search button
+input.addEventListener('keypress', handleEnter);
+function handleEnter(event) {
+    if (event.key === 'Enter') {
+        search();
+    }
+}
+
+let randomNum = Math.floor(Math.random() * hikes.length);
+console.log(randomNum);
+
+function tagTemplate(tags) {
+    return tags.map((tag)=> `<button>${tag}</button>`).join(' ');
+}
+
+function difficultyTemplate(rating) {
+    let html = `<span
+	class="rating"
+	role="img"
+	aria-label="Rating: ${rating} out of 5"
+>  Difficulty: `
+    for (let i = 1; i <= 5; i++) {
+        if (i <= rating) {
+            html += `<span aria-hidden="true" class="icon-boot">\u{1F97E} </span>`
+        } else {
+            html += `<span aria-hidden="true" class="icon-empty">\u{25FB}\uFE0F</span>`
+        }
+    }
+    html += `</span>`
+    return html
+}
+
+function hikesTemplate(hike) {
+    return `<div class="hike-card">
+  <div class="hike-content">
+    <h2>${hike.name}</h2>
+    <div class="hike-tags">
+      ${tagTemplate(hike.tags)}
+    </div>
+    <p>${hike.description}</p>
+    <p>${difficultyTemplate(hike.difficulty)}</p>
+  </div>
+</div>`
+}
+
+function renderHike(hike) {
+    let html = hikesTemplate(hike);
+    hikeContainer.innerHTML += html
+}
+
+function init() {
+    renderHike(hikes[randomNum]);
+}
+
+init();
+
